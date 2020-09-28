748b9b586e8c593d9a093cadc3a1fbe9578b353d
David <drgomez92@gmail.com>
2020-09-28 18:23:39 +0200

    Finalizado. Añadido CSS al resultado. Añadido un Spinner para que espere la respuesta de la API.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Axios from 'axios'
 import imagen from './cryptomonedas.png'
 import Formulario from './components/Formulario'
 import Cotizacion from './components/Cotizacion'
+import Spinner from './components/Spinner'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -43,6 +44,7 @@ function App() {
   const [moneda, guardarMoneda] = useState('')
   const [criptomoneda, guardarCriptomoneda] = useState('')
   const [resultado, guardarResultado] = useState({})
+  const [cargando, guardarCargando] = useState(false)
 
   //Segunda llamada a la API para conocer la cotización, se ejecuta cuando los estados de moneda y criptomoneda se actualizan
   useEffect(() => {
@@ -53,13 +55,22 @@ function App() {
   
       const resultado = await Axios.get(url)
 
-      //Debido a que los campos del objeto que devuelve la API cambian según las opciones que el usuario elija, es necesario definir los campos de criptomoneda y moneda de esta manera, para poder acceder a ellos (respuesta de API dinámica)
-      guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda])
+      //Mostrar el spinner
+      guardarCargando(true)
+
+      setTimeout(() => {
+          guardarCargando(false)
+
+          //Debido a que los campos del objeto que devuelve la API cambian según las opciones que el usuario elija, es necesario definir los campos de criptomoneda y moneda de esta manera, para poder acceder a ellos (respuesta de API dinámica)
+          guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda])
+      }, 3000)  
     }
 
     cotizarCriptomoneda()
 
   }, [moneda, criptomoneda])
+
+  const Componente = (cargando) ? <Spinner /> : <Cotizacion resultado={resultado} />
 
   return (
     <Contenedor>
@@ -71,7 +82,7 @@ function App() {
 
         <Formulario guardarMoneda={guardarMoneda} guardarCriptomoneda={guardarCriptomoneda} />
 
-        <Cotizacion resultado={resultado} />
+        {Componente}
       </div>
     </Contenedor>
   )
